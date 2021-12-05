0f6c813c60711940d6c80771c3ad5ed02e5fc3c9
Mohammad Salah <40919421+SherlokMo@users.noreply.github.com>
2021-12-05 21:22:40 +0200

    Added pseudocode to palindrom iterative

--- lecture3/slide36-palindrom-iterative.go
+++ lecture3/slide36-palindrom-iterative.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+/**
+	N is the length of string st
+
+	Algorithm isPalindrom(st, n):
+		l <- 0
+		r <- n - 1
+		while l < r
+			if st[l] != st[r]:
+				return false
+			r <- r - 1
+			l <- l + 1
+		endwhile
+
+		return true
+**/
+
 func palindromeIterative(st string) bool {
 
 	for l, r := 0, len(st)-1; l < r; {
